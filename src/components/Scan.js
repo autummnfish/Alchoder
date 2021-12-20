f334282c558eed690f7090e25dbfb30b24612fbc
sanma-mod <mrstrandjpn@gmail.com>
2021-12-21 01:26:11 +0900

    quaggajsから新しいライブラリへ変更

--- src/components/Scan.js
+++ src/components/Scan.js
@@ -1,38 +1,15 @@
 import { useIonAlert } from "@ionic/react";
 import Quagga from "quagga";
-import { useState,useEffect } from "react";
-import { fetchItemInfomation,fetchItemFormat } from "../api";
+import { useState, useEffect } from "react";
+import { fetchItemInfomation, fetchItemFormat } from "../api";
 
 const Scan = (props) => {
-
   const [barcode, setBarcode] = useState("4901411086798"); //このjanコードはダミーデータのようなもの
   const [present] = useIonAlert();
 
-  const addDrinkLog = (value) => {
-    props.addLog(value);
-  }
 
-  const reloadItemName = (code) => {
-    fetchItemInfomation(code).then((rawName) => {
-      setBarcode(code);
-      if (rawName !== "" && rawName != null) {
-        present({
-          header: "このバーコードを登録しますか？",
-          message: `${code}`,
-          buttons: [
-            "Cancel",
-            { text: "Ok", handler: () => {
-              fetchItemFormat(rawName).then((formatName) => {
-                addDrinkLog(formatName);
-              })
-            } },
-          ],
-        });
-      }
-    });
-  };
   const cameraSize = Math.min(window.innerHeight, window.innerWidth);
-  const webWorker = Math.min(navigator.hardwareConcurrency , 4)
+  const webWorker = Math.min(navigator.hardwareConcurrency, 4);
   const config = {
     //カメラ設定
     inputStream: {
@@ -65,32 +42,57 @@ const Scan = (props) => {
   };
 
   useEffect(() => {
-    onChangeQuaggaCamera();
+    const addDrinkLog = (value) => {
+      props.addLog(value);
+    };
+    
+    const reloadItemName = (code) => {
+      fetchItemInfomation(code).then((rawName) => {
+        setBarcode(code);
+        if (rawName !== "" && rawName != null) {
+          present({
+            header: "このバーコードを登録しますか？",
+            message: `${code}`,
+            buttons: [
+              "Cancel",
+              {
+                text: "Ok",
+                handler: () => {
+                  fetchItemFormat(rawName).then((formatName) => {
+                    addDrinkLog(formatName);
+                  });
+                },
+              },
+            ],
+          });
+        }
+      });
+    };
+
+    const onChangeQuaggaCamera = () => {
+      Quagga.init(config, (err) => {
+        if (err) {
+          present({
+            header: "カメラを許可してください",
+            buttons: ["OK"],
+          });
+          return;
+        }
+        Quagga.start();
+      });
+    };
     Quagga.onDetected((result) => {
       //誤認識を防ぐために複数回バーコードが一致する場合としてもよさそう
       if (result != null) {
         setTimeout(reloadItemName(result.codeResult.code), 1000);
       }
     });
+    onChangeQuaggaCamera();
   }, []);
-  const onChangeQuaggaCamera = () => {
-    Quagga.init(config, (err) => {
-      if (err) {
-        present({
-          header: "カメラを許可してください",
-          buttons: [
-            "OK",
-          ],
-        })
-        return;
-      }
-      Quagga.start();
-    });
-  };
 
   return (
     <div>
-      <div>読み込んでいるやつ：{barcode}</div>
+      <div>読み込んでいるやつ : {barcode}</div>
       <div id="preview"></div>
     </div>
   );
